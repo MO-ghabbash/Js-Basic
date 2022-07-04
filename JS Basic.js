b1c6f82c071eb922f13d3c6dee715a51dbedd2cb
Muhannad <seefsy@gmail.com>
2022-07-04 16:31:32 +0200

    first commit

---
+++ JS Basic.js
@@ -0,0 +1,59 @@
+// for (var i = 1; i <= 135; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i <= 135; i++) {
+//   if (i % 2 != 0) console.log(i);
+// }
+
+// let num = 1;
+// let sum = 0;
+// let vob = 0;
+// while (num != 135) {
+//   sum = num + vob;
+// }
+//   console.log(num);
+//   vob = num;
+//   num += 1;
+//   console.log("Number is:" + num + "sum :" + sum);
+
+// var x = [1, 4, 2, 12];
+// console.log(x);
+
+// let x = Math.max(2, -3, -1);
+// console.log(x);
+
+// x = [2, 1, 3];
+
+// let sum = 0;
+// for (let i = 0; i < x.length; i++) {
+//   sum = sum + x[i];
+// }
+// console.log(sum / x.length);
+
+// function elimination(eliminate) {
+//   var result = [];
+//   for (var i = 0; i < eliminate.length; i++)
+//     if (eliminate[i] < 0) {
+//       result.push(0);
+//     } else {
+//       result.push(eliminate[i]);
+//     }
+//   console.log(result);
+// }
+
+// var x = [2, -1, 4, -3];
+// elimination(x);
+
+// function numberToString(array) {
+//   var finel = [];
+//   for (var i = 0; i < array.length; i++)
+//     if (array[i] < 0) {
+//       finel.push("turing");
+//     } else {
+//       finel.push(array[i]);
+//     }
+//   console.log(finel);
+// }
+//     var z = [1, -4, 0, -1];
+//     numberToString(z);
